b4347066beb7ff7610a56e012809692275573619
yagolc <yagolimacapistrano@alu.ufc.br>
2020-11-24 09:59:50 -0300

    POO3

---
+++ POO3/src/poo3/POO3.java
@@ -0,0 +1,17 @@
+
+package poo3;
+
+/**
+ *
+ * @author yagolS
+ */
+public class POO3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            System.out.println("oi mundo");
+    }
+   
+}
